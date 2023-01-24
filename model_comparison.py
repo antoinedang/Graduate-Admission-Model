5344d24d4f591e43e86e6625c10e452ba8c6ca42
antoinedang <antoine.dangeard@gmail.com>
2023-01-24 01:19:35 -0500

    Finished model comparison script
    Fairly straight forward model ranking for now, uses k-folds and data shuffling to find most generalizable model since final evaluation test data is not available, might implement data visualization if time permits

--- model_comparison.py
+++ model_comparison.py
@@ -1,49 +1,70 @@
-from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR, NuSVR
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.pipeline import Pipeline
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.ensemble import GradientBoostingRegressor
-from xgboost import XGBRegressor
-from catboost import CatBoostRegressor
-from sklearn.linear_model import Lasso,Ridge,BayesianRidge,ElasticNet,HuberRegressor,LinearRegression,LogisticRegression,SGDRegressor
+from sklearn.linear_model import Lars,Lasso,LassoLars,Ridge,BayesianRidge,ElasticNet,HuberRegressor,LinearRegression,SGDRegressor,PassiveAggressiveRegressor,QuantileRegressor,ARDRegression,OrthogonalMatchingPursuit
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
+from sklearn.neural_network import MLPRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.utils import shuffle
+import os
+import numpy as np
 
 if __name__ == '__main__':
+    pwd = os.path.realpath(os.path.dirname(__file__))
+    x_csv = pwd + '/data/clean/data_x.csv'
+    X = []
+    with open(x_csv, "r") as f:
+        for entry in f.read().split('\n'):
+            X.append(list(map(float, entry.split(","))))
+    X = StandardScaler().fit_transform(X)
+    y_csv = pwd + '/data/clean/data_y.csv'
+    Y = []
+    with open(y_csv, "r") as f:
+        for entry in f.read().split('\n'):
+            Y.append(float(entry))
 
-    models = [['DecisionTree :',DecisionTreeRegressor()],
-            ['Linear Regression :', LinearRegression()],
-            ['RandomForest :',RandomForestRegressor()],
-            ['KNeighbours :', KNeighborsRegressor(n_neighbors = 2)],
-            ['SVM :', SVR()],
-            ['AdaBoostClassifier :', AdaBoostRegressor()],
-            ['GradientBoostingClassifier: ', GradientBoostingRegressor()],
-            ['Xgboost: ', XGBRegressor()],
-            ['CatBoost: ', CatBoostRegressor(logging_level='Silent')],
+    scoring = "neg_mean_squared_error"
+    k_folds = 7 #number of folds to cut data into for training
+    num_shuffles = 10 #number of times to score models (data shuffled every time)
+    print("Initializing Models...")
+    models = [
+            ['DecisionTreeRegressor :',DecisionTreeRegressor()],
+            ['LinearRegression :', LinearRegression()],
+            ['PassiveAggressiveRegressor :', PassiveAggressiveRegressor()],
+            ['OrthogonalMatchingPursuit :', OrthogonalMatchingPursuit(normalize=False)],
+            ['MLPRegressor :', MLPRegressor()],
+            ['SGDRegressor :', SGDRegressor()],
+            ['RandomForestRegressor :', RandomForestRegressor()],
+            ['KNeighborsRegressor :', KNeighborsRegressor()],
+            ['SVR :', SVR()],
+            ['NuSVR :', NuSVR()],
+            ['AdaBoostRegressor :', AdaBoostRegressor()],
+            ['GradientBoostingRegressor: ', GradientBoostingRegressor()],
+            ['PLSRegression: ', PLSRegression()],
             ['Lasso: ', Lasso()],
+            ['Lars: ', Lars(normalize=False)],
+            ['LassoLars: ', LassoLars(normalize=False)],
+            ['QuantileRegressor: ', QuantileRegressor()],
+            ['ARDRegression: ', ARDRegression()],
             ['Ridge: ', Ridge()],
+            ['KernelRidge: ', KernelRidge()],
             ['BayesianRidge: ', BayesianRidge()],
             ['ElasticNet: ', ElasticNet()],
             ['HuberRegressor: ', HuberRegressor()]]
 
-    X = 
-    
-    k_folds = 5
-    scoring = "neg_mean_squared_error"
-    #neg_mean_absolute_error
-    #neg_mean_squared_error
-    #neg_root_mean_squared_error
-    #neg_mean_squared_log_error
-
-    clf = svm.SVC(kernel='linear', C=1, random_state=42)
-    scores = cross_val_score(clf, X, y, cv=k_folds)
-
-    print("Results...")
-    for name,model in models:
-        model = model
-        model.fit(X_train, y_train)
-        predictions = model.predict(X_test)
-        print(name, (np.sqrt(mean_squared_error(y_test, predictions))))
+    worst_scores = {} #save worst score for each model over all attempts
+    for i in range(num_shuffles):
+        X, Y = shuffle(X, Y)
+        for j in range(len(models)):
+            name, model = models[j]
+            scores = cross_val_score(model, X, Y, cv=k_folds, scoring=scoring)
+            worst_scores[name] = min(min(scores), worst_scores.get(name, 0))
+            print("Scoring models... " + str(100*((j+1)+(i*len(models)))/(len(models)*10)) + "%")
+    print("Final results: \n" + str(worst_scores))
